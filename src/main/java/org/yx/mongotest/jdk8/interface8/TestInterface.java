fb8fd3392aaa3bf5073ffd18a034ec7993952955
yangxin <943332358@qq.com>
2021-08-05 17:46:32 +0800

    jdk1.8相关

--- src/main/java/org/yx/mongotest/jdk8/interface8/TestInterface.java
+++ src/main/java/org/yx/mongotest/jdk8/interface8/TestInterface.java
@@ -2,12 +2,14 @@ package org.yx.mongotest.jdk8.interface8;
 
 import lombok.var;
 
+import static org.yx.mongotest.jdk8.interface8.Obj.run;
+
 /**
  * @author yangxin
  */
 public class TestInterface {
     public static void main(String[] args) {
-        Obj.run();
+        run();
 
         var cat = new Cat();
         cat.say();
