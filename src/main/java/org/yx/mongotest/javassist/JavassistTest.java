da3d47bd053b8d07c6bf471d2e751a66c3ccda02
yangxin <943332358@.qq.com>
2021-09-30 18:12:43 +0800

    Default Changelist

--- src/main/java/org/yx/mongotest/javassist/JavassistTest.java
+++ src/main/java/org/yx/mongotest/javassist/JavassistTest.java
@@ -1,10 +1,28 @@
 package org.yx.mongotest.javassist;
 
+import org.yx.mongotest.authorization.entity.User;
+
 /**
  * @author yangxin
  */
 public class JavassistTest {
+    private String name;
+
+    public JavassistTest(String name) {
+        this.name = name;
+    }
+
     public void say() {
         System.out.println("Hello");
+    }
+
+    public void say(String aa) {
+        System.out.println(aa);
+    }
+
+    public void say(String aa, User user) {
+        String sql = "insert into aa";
+        System.out.println("sql：" + sql);
+        System.out.println(aa + user);
     }
 }
--- src/main/java/org/yx/mongotest/javassist/Test.java
+++ src/main/java/org/yx/mongotest/javassist/Test.java
@@ -3,6 +3,8 @@ package org.yx.mongotest.javassist;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import lombok.var;
+import org.yx.mongotest.authorization.entity.User;
 
 /**
  * @author yangxin
@@ -10,24 +12,29 @@ import javassist.CtMethod;
 public class Test {
     public static void main(String[] args) throws Exception {
         ClassPool pool = ClassPool.getDefault();
-        String cc1 = "cc";
-        System.out.println(cc1);
 
 
         CtClass cc = pool.get("org.yx.mongotest.javassist.JavassistTest");
         cc.defrost();
 
+        var userClass = pool.get("org.yx.mongotest.authorization.entity.User");
+        var stringClass = pool.get("java.lang.String");
+        var m = cc.getDeclaredMethod("say", new CtClass[]{stringClass, userClass});
 
-        CtMethod m = cc.getDeclaredMethod("say");
+
         m.insertBefore("System.out.println(\"modify class ok\");");
         m.insertAfter("System.out.println(\"modify class end\");");
-        m.insertAt(9, "int a = 10;" +
-                "System.out.println(a);");
+        m.insertAt(26, "int a = 10;" +
+                "System.out.println(a);" +
+                "System.out.println(name);");
+
+        m.insertAt(25, "sql = \"update aa\";");
 
         cc.writeFile();
         cc.toClass();
 
-        JavassistTest h2 = new JavassistTest();
-        h2.say();
+        JavassistTest h2 = new JavassistTest("yangxin");
+        h2.say("aa", new User());
+
     }
 }
